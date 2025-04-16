fd920ea041c934f7839271cf6b8daff483226731
Jaheim Caesar <craftingfan101@gmail.com>
2025-04-16 17:20:16 -0400

    wordRareness

--- Module/wordRareness.py
+++ Module/wordRareness.py
@@ -1,10 +1,11 @@
 from wordfreq import word_frequency
+import re
 
 def calculate_word_rareness_wordfreq(word, lang='en'):
 
     frequency = word_frequency(word, lang)
     if frequency == 0:
-        return float('inf') 
+        return 6
     else:
         rareness_score = 1 / frequency
         scaled_score = min(6, 0.10 + (rareness_score / 1000000)) 
@@ -12,21 +13,15 @@ def calculate_word_rareness_wordfreq(word, lang='en'):
 
 def text_rareness_score(text, lang='en'):
 
-    words = sorted(list(set(text.lower().split()))) 
+    words = sorted(set(re.findall(r'\b\w+\b', text.lower())))
     rareness_scores = []
-    valid_rareness_values = []
 
     for word in words:
         rareness = calculate_word_rareness_wordfreq(word, lang)
-        rareness_output = "Infinity" if rareness == float('inf') else rareness
-        rareness_scores.append((word, rareness_output))
-        # rareness_scores.append((word, rareness)) 
-        if rareness != float('inf'):
-            valid_rareness_values.append(rareness)
+        rareness_scores.append((word, rareness))
 
-    if valid_rareness_values:
-        average_rareness = sum(valid_rareness_values) / len(valid_rareness_values)
+    if rareness_scores:
+        average_rareness = sum(score for _, score in rareness_scores) / len(rareness_scores)
         return average_rareness, rareness_scores
     else:
         return 0, rareness_scores
-
